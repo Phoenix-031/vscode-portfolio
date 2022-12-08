b1ad8e7ec22fffd74ed94668758c0396fd6c83d8
Debayan <pradhandebayan@gmail.com>
2022-12-08 21:21:41 +0530

    updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import profileimg from '../assets/profile2.jpg'
 import  {motion} from 'framer-motion'
-import { useState } from 'react'
+// import { useState } from 'react'
 
 const About = () => {
 
-  const [loaded,setLoaded] = useState(false)
+  // const [loaded,setLoaded] = useState(false)
   
   return (
     <motion.div className='row-start-2 bg-drk00 h-full overflow-auto' 
